93a9788cad3cf042ea74ab99531394e52a379b94
yashkumar0707 <yashkumar0707@gmail.com>
2021-04-09 11:01:42 +0530

    Updating Spawn Manager for AR

--- Scripts/SpawnManager.cs
+++ Scripts/SpawnManager.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client;
 public class SpawnManager : MonoBehaviourPunCallbacks
 {
 
     public GameObject[] playerPrefabs;
     public Transform[] spawnPositions;
+    public GameObject battleArenaGameObject;
+
+    public enum RaiseEventCodes
+    {
+        PlayerSpawnEventCode = 0
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -24,15 +32,59 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     {
         if(PhotonNetwork.IsConnectedAndReady)
         {
-            object playerSelectionNumber;
-            if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARSpinnerTopGame.PLAYER_SELECTION_NUMBER, out playerSelectionNumber)) 
+            //object playerSelectionNumber;
+            //if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARSpinnerTopGame.PLAYER_SELECTION_NUMBER, out playerSelectionNumber)) 
+            //{
+            //    Debug.Log("Player selection number " + (int)playerSelectionNumber);
+            //    int randomSpawnPoint = Random.Range(0, spawnPositions.Length - 1);
+            //    Vector3 instantiatePosition = spawnPositions[randomSpawnPoint].position;
+            //    PhotonNetwork.Instantiate(playerPrefabs[(int)playerSelectionNumber].name,instantiatePosition,Quaternion.identity);
+            //}
+            SpawnPlayer();
+
+        }
+    }
+    #endregion
+
+    #region Private Methods
+    private void SpawnPlayer()
+    {
+        object playerSelectionNumber;
+        if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARSpinnerTopGame.PLAYER_SELECTION_NUMBER, out playerSelectionNumber))
+        {
+            Debug.Log("Player selection number " + (int)playerSelectionNumber);
+            int randomSpawnPoint = Random.Range(0, spawnPositions.Length - 1);
+            Vector3 instantiatePosition = spawnPositions[randomSpawnPoint].position;
+
+            GameObject playerGameObject = instantiatePosition(playerPrefabs[(int)playerSelectionNumber], instantiatePosition, Quaternion.identity);
+
+            PhotonView _photonView = playerGameObject.GetComponent<PhotonView>();
+            if(PhotonNetwork.AllocateViewID(_photonView))
             {
-                Debug.Log("Player selection number " + (int)playerSelectionNumber);
-                int randomSpawnPoint = Random.Range(0, spawnPositions.Length - 1);
-                Vector3 instantiatePosition = spawnPositions[randomSpawnPoint].position;
-                PhotonNetwork.Instantiate(playerPrefabs[(int)playerSelectionNumber].name,instantiatePosition,Quaternion.identity);
+                object[] data = new object[]
+                {
+                    playerGameObject.transform.position - battleArenaGameObject.transform.position, playerGameObject.transform.rotation, _photonView.ViewID, playerSelectionNumber
+                };
+
+                RaiseEventOptions raiseEventOptions = new RaiseEventOptions
+                {
+                    Receivers = ReceiverGroup.Others,
+                    CatchingOption = EventCatching.AddToRoomCache
+                };
+                SendOptions sendOptions = new SendOptions
+                {
+                    Reliability = true
+                };
+                //Raise Events
+                PhotonNetwork.RaiseEvent((byte)RaiseEventCodes.PlayerSpawnEventCode, data, raiseEventOptions, sendOptions);
+            }
+            else
+            {
+                Debug.Log("Failed to allocate a View ID");
+                Destroy(playerGameObject);
             }
         }
     }
     #endregion
+
 }
