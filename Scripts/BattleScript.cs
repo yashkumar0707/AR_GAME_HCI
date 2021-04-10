b7d2407d44b6c97d0cc5daf64554412d7a51599c
yashkumar0707 <yashkumar0707@gmail.com>
2021-04-10 18:44:16 +0530

    Final apk with sound and visual effects

--- Scripts/BattleScript.cs
+++ Scripts/BattleScript.cs
@@ -1,63 +1,292 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Photon.Pun;
+//using TMPro;
+//public class BattleScript : MonoBehaviourPun
+//{
+//    public Spinner spinnerScript;
+//    public GameObject uI_3D_GameObject;
+//    private Rigidbody rb;
+
+//    public GameObject deathPanelUIPrefab;
+//    private GameObject deathPanelUIGameObject;
+
+//    private float startSpinSpeed;
+//    private float currentSpinSpeed;
+//    public Image spinSpeedBar_Image;
+//    public TextMeshProUGUI spinSpeedRatio_Text;
+
+//    public float common_Damage_Cofficient = 0.04f;
+//    public bool isAttacker;
+//    public bool isDefender;
+//    private bool isDead = false;
+//    [Header("Player Type Damage Coefficients")]
+//    public float doDamage_Coefficient_Attacker = 10f;
+//    public float getDamaged_Coefficient_Attacker = 1.2f;
+
+//    public float doDamage_Coefficient_Defender = 0.75f;
+//    public float getDamaged_Coefficient_Defender = 0.2f;
+//    // Start is called before the first frame update
+
+//    private void Awake()
+//    {
+//        startSpinSpeed = spinnerScript.spinSpeed;
+//        currentSpinSpeed = spinnerScript.spinSpeed;
+//        spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
+//    }
+
+//    private void CheckPlayerType()
+//    {
+//        if(gameObject.name.Contains("Attacker"))
+//        {
+//            isAttacker = true;
+//            isDefender = false;
+//        }
+//        else if(gameObject.name.Contains("Defender"))
+//        {
+//            isDefender = true;
+//            isAttacker = false;
+//            spinnerScript.spinSpeed = 4400;
+//            startSpinSpeed = spinnerScript.spinSpeed;
+//            currentSpinSpeed = spinnerScript.spinSpeed;
+//            spinSpeedRatio_Text.text = currentSpinSpeed + "/" + startSpinSpeed;
+//        }
+//    }
+
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        if(collision.gameObject.CompareTag("Player"))
+//        {
+//            if (photonView.IsMine)
+//            {
+//                Vector3 effectPosition = (gameObject.transform.position + collision.transform.position) / 2 + new Vector3(0, 0.05f, 0);
+
+//                //Instantiate Collision Effect ParticleSystem
+//                GameObject collisionEffectGameobject = GetPooledObject();
+//                if (collisionEffectGameobject != null)
+//                {
+//                    collisionEffectGameobject.transform.position = effectPosition;
+//                    collisionEffectGameobject.SetActive(true);
+//                    collisionEffectGameobject.GetComponentInChildren<ParticleSystem>().Play();
+
+//                    //De-activate Collision Effect Particle System after some seconds.
+//                    StartCoroutine(DeactivateAfterSeconds(collisionEffectGameobject, 0.5f));
+
+//                }
+//            }
+//            float mySpeed = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+//            float otherPlayerSpeed = collision.collider.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+
+//            Debug.Log("My Speed: " + mySpeed + "Other Player Speed: " + otherPlayerSpeed);
+//            if(mySpeed > otherPlayerSpeed)
+//            {
+//                float default_Damage_Amount = gameObject.GetComponent<Rigidbody>().velocity.magnitude * 3600f * common_Damage_Cofficient;
+//                Debug.Log("You damage other");
+//                if (isAttacker)
+//                {
+//                    default_Damage_Amount = default_Damage_Amount * doDamage_Coefficient_Attacker;
+
+//                }
+//                else if (isDefender)
+//                {
+//                    default_Damage_Amount = default_Damage_Amount * doDamage_Coefficient_Defender;
+//                }
+
+//                if (collision.collider.gameObject.GetComponent<PhotonView>().IsMine)
+//                {
+
+//                    collision.collider.gameObject.GetComponent<PhotonView>().RPC("DoDamage", RpcTarget.AllBuffered, default_Damage_Amount);
+
+//                }
+//            }
+
+//        }
+//    }
+
+//    [PunRPC]
+//    public void DoDamage(float _damageAmount)
+//    {   
+//        if(!isDead)
+//        {
+//            if (isAttacker)
+//            {
+//                _damageAmount *= getDamaged_Coefficient_Attacker;
+
+//                if(_damageAmount>500)
+//                {
+//                    Debug.Log("TRUEEE");
+//                    _damageAmount = 400f;
+//                }
+//            }
+//            else if (isDefender)
+//            {
+//                _damageAmount *= getDamaged_Coefficient_Defender;
+//            }
+//            spinnerScript.spinSpeed -= _damageAmount;
+//            currentSpinSpeed = spinnerScript.spinSpeed;
+//            spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
+//            spinSpeedRatio_Text.text = currentSpinSpeed.ToString("F0") + "/" + startSpinSpeed;
+//            Debug.Log(spinSpeedRatio_Text.text + " " + startSpinSpeed);
+//            if (currentSpinSpeed < 100)
+//            {
+//                //Die
+//                Die();
+//            }
+//        }
+
+//    }
+//    void Die()
+//    {
+//        isDead = true;
+//        GetComponent<MovementController>().enabled = false;
+//        rb.freezeRotation = false;
+//        rb.velocity = Vector3.zero;
+//        rb.angularVelocity = Vector3.zero;
+//        spinnerScript.spinSpeed = 0f;
+//        uI_3D_GameObject.SetActive(false);
+
+//        if(photonView.IsMine)
+//        {
+//            StartCoroutine(ReSpawn());
+//        }
+//    }
+//    IEnumerator ReSpawn()
+//    {
+//        GameObject canvasGameObject = GameObject.Find("Canvas");
+//        if (deathPanelUIGameObject == null)
+//        {
+//            deathPanelUIGameObject = Instantiate(deathPanelUIPrefab, canvasGameObject.transform);
+//        }
+//        else
+//        {
+//            deathPanelUIGameObject.SetActive(true);
+//        }
+//        Text reSpawnTimeText = deathPanelUIGameObject.transform.Find("RespawnTimeText").GetComponent<Text>();
+
+//        float respawnTime = 8.0f;
+
+//        reSpawnTimeText.text = respawnTime.ToString(".00");
+//        while(respawnTime>0.0f)
+//        {
+//            yield return new WaitForSeconds(1.0f);
+//            respawnTime -= 1.0f;
+//            reSpawnTimeText.text = respawnTime.ToString(".00");
+
+//            GetComponent<MovementController>().enabled = false;
+//        }
+
+//        deathPanelUIGameObject.SetActive(false);
+//        GetComponent<MovementController>().enabled = true;
+//        photonView.RPC("ReBorn", RpcTarget.AllBuffered);
+
+//    }
+//    [PunRPC]
+//    public void ReBorn()
+//    {
+//        spinnerScript.spinSpeed = startSpinSpeed;
+//        currentSpinSpeed = spinnerScript.spinSpeed;
+
+//        spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
+//        spinSpeedRatio_Text.text = currentSpinSpeed + "/" + startSpinSpeed;
+//        rb.freezeRotation = true;
+//        transform.rotation = Quaternion.Euler(Vector3.zero);
+//        uI_3D_GameObject.SetActive(true);
+//        isDead = false; 
+//    }
+//    void Start()
+//    {
+//        CheckPlayerType();
+//        rb = GetComponent<Rigidbody>();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
+
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
+
 public class BattleScript : MonoBehaviourPun
 {
     public Spinner spinnerScript;
-    public GameObject uI_3D_GameObject;
-    private Rigidbody rb;
 
+    public GameObject uI_3D_Gameobject;
     public GameObject deathPanelUIPrefab;
-    private GameObject deathPanelUIGameObject;
+    private GameObject deathPanelUIGameobject;
+
+
+    private Rigidbody rb;
 
     private float startSpinSpeed;
     private float currentSpinSpeed;
     public Image spinSpeedBar_Image;
     public TextMeshProUGUI spinSpeedRatio_Text;
 
-    public float common_Damage_Cofficient = 0.04f;
+
+    public float common_Damage_Coefficient = 0.04f;
+
     public bool isAttacker;
     public bool isDefender;
     private bool isDead = false;
-    [Header("Player Type Damage Coefficients")]
-    public float doDamage_Coefficient_Attacker = 10f;
-    public float getDamaged_Coefficient_Attacker = 1.2f;
 
-    public float doDamage_Coefficient_Defender = 0.75f;
-    public float getDamaged_Coefficient_Defender = 0.2f;
-    // Start is called before the first frame update
+
+    [Header("Player Type Damage Coefficients")]
+    public float doDamage_Coefficient_Attacker = 10f; //do more damage than defender- ADVANTAGE
+    public float getDamaged_Coefficient_Attacker = 1.2f;// gets more damage - DISADVANTAGE
+
+    public float doDamage_Coefficient_Defender = 0.75f; //do less damage- DISADVANTAGE
+    public float getDamaged_Coefficient_Defender = 0.2f; //gets less damage - ADVANTAGE
+
 
     private void Awake()
     {
         startSpinSpeed = spinnerScript.spinSpeed;
         currentSpinSpeed = spinnerScript.spinSpeed;
+
         spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
+
     }
 
     private void CheckPlayerType()
     {
-        if(gameObject.name.Contains("Attacker"))
+        if (gameObject.name.Contains("Attacker"))
         {
             isAttacker = true;
             isDefender = false;
+
+
         }
-        else if(gameObject.name.Contains("Defender"))
+        else if (gameObject.name.Contains("Defender"))
         {
             isDefender = true;
             isAttacker = false;
+
             spinnerScript.spinSpeed = 4400;
+
             startSpinSpeed = spinnerScript.spinSpeed;
             currentSpinSpeed = spinnerScript.spinSpeed;
+
             spinSpeedRatio_Text.text = currentSpinSpeed + "/" + startSpinSpeed;
+
         }
     }
 
+
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             if (photonView.IsMine)
             {
@@ -76,134 +305,205 @@ public class BattleScript : MonoBehaviourPun
 
                 }
             }
+            //Comparing the speeds of the SPinnerTops
             float mySpeed = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
             float otherPlayerSpeed = collision.collider.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
 
-            Debug.Log("My Speed: " + mySpeed + "Other Player Speed: " + otherPlayerSpeed);
-            if(mySpeed > otherPlayerSpeed)
+            Debug.Log("My speed: " + mySpeed + " -----Other player speed: " + otherPlayerSpeed);
+
+            if (mySpeed > otherPlayerSpeed)
             {
-                float default_Damage_Amount = gameObject.GetComponent<Rigidbody>().velocity.magnitude * 3600f * common_Damage_Cofficient;
-                Debug.Log("You damage other");
+                Debug.Log(" You DAMAGE the other player.");
+                float default_Damage_Amount = gameObject.GetComponent<Rigidbody>().velocity.magnitude * 3600f * common_Damage_Coefficient;
+
                 if (isAttacker)
                 {
-                    default_Damage_Amount = default_Damage_Amount * doDamage_Coefficient_Attacker;
+                    default_Damage_Amount *= doDamage_Coefficient_Attacker;
 
                 }
                 else if (isDefender)
                 {
-                    default_Damage_Amount = default_Damage_Amount * doDamage_Coefficient_Defender;
+                    default_Damage_Amount *= doDamage_Coefficient_Defender;
                 }
 
                 if (collision.collider.gameObject.GetComponent<PhotonView>().IsMine)
                 {
-                    
+                    //Apply Damage to the slower player.
                     collision.collider.gameObject.GetComponent<PhotonView>().RPC("DoDamage", RpcTarget.AllBuffered, default_Damage_Amount);
-
                 }
             }
-     
         }
     }
 
+
     [PunRPC]
     public void DoDamage(float _damageAmount)
-    {   
-        if(!isDead)
+    {
+        if (!isDead)
         {
             if (isAttacker)
             {
                 _damageAmount *= getDamaged_Coefficient_Attacker;
 
-                if(_damageAmount>500)
+                if (_damageAmount > 1000)
                 {
-                    Debug.Log("TRUEEE");
                     _damageAmount = 400f;
                 }
+
             }
             else if (isDefender)
             {
+
                 _damageAmount *= getDamaged_Coefficient_Defender;
             }
             spinnerScript.spinSpeed -= _damageAmount;
             currentSpinSpeed = spinnerScript.spinSpeed;
+
             spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
             spinSpeedRatio_Text.text = currentSpinSpeed.ToString("F0") + "/" + startSpinSpeed;
-            Debug.Log(spinSpeedRatio_Text.text + " " + startSpinSpeed);
+
             if (currentSpinSpeed < 100)
             {
                 //Die
                 Die();
             }
         }
-       
+
     }
+
     void Die()
     {
+
         isDead = true;
+
         GetComponent<MovementController>().enabled = false;
         rb.freezeRotation = false;
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
-        spinnerScript.spinSpeed = 0f;
-        uI_3D_GameObject.SetActive(false);
 
-        if(photonView.IsMine)
+        spinnerScript.spinSpeed = 0f;
+
+        uI_3D_Gameobject.SetActive(false);
+
+
+        if (photonView.IsMine)
         {
+            //countdown for respawn
             StartCoroutine(ReSpawn());
         }
+
+
     }
+
     IEnumerator ReSpawn()
     {
-        GameObject canvasGameObject = GameObject.Find("Canvas");
-        if (deathPanelUIGameObject == null)
+        GameObject canvasGameobject = GameObject.Find("Canvas");
+        if (deathPanelUIGameobject == null)
         {
-            deathPanelUIGameObject = Instantiate(deathPanelUIPrefab, canvasGameObject.transform);
+            deathPanelUIGameobject = Instantiate(deathPanelUIPrefab, canvasGameobject.transform);
+
         }
         else
         {
-            deathPanelUIGameObject.SetActive(true);
+            deathPanelUIGameobject.SetActive(true);
         }
-        Text reSpawnTimeText = deathPanelUIGameObject.transform.Find("RespawnTimeText").GetComponent<Text>();
+
+        Text respawnTimeText = deathPanelUIGameobject.transform.Find("RespawnTimeText").GetComponent<Text>();
 
         float respawnTime = 8.0f;
 
-        reSpawnTimeText.text = respawnTime.ToString(".00");
-        while(respawnTime>0.0f)
+        respawnTimeText.text = respawnTime.ToString(".00");
+
+        while (respawnTime > 0.0f)
         {
             yield return new WaitForSeconds(1.0f);
             respawnTime -= 1.0f;
-            reSpawnTimeText.text = respawnTime.ToString(".00");
+            respawnTimeText.text = respawnTime.ToString(".00");
 
             GetComponent<MovementController>().enabled = false;
+
         }
 
-        deathPanelUIGameObject.SetActive(false);
+        deathPanelUIGameobject.SetActive(false);
+
         GetComponent<MovementController>().enabled = true;
+
         photonView.RPC("ReBorn", RpcTarget.AllBuffered);
 
+
     }
+
     [PunRPC]
     public void ReBorn()
     {
         spinnerScript.spinSpeed = startSpinSpeed;
         currentSpinSpeed = spinnerScript.spinSpeed;
 
+
         spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
         spinSpeedRatio_Text.text = currentSpinSpeed + "/" + startSpinSpeed;
+
         rb.freezeRotation = true;
         transform.rotation = Quaternion.Euler(Vector3.zero);
-        uI_3D_GameObject.SetActive(true);
-        isDead = false; 
+
+        uI_3D_Gameobject.SetActive(true);
+
+        isDead = false;
     }
+
+
+
+
+
+
+
+
+    public List<GameObject> pooledObjects;
+    public int amountToPool = 8;
+    public GameObject CollisionEffectPrefab;
+    // Start is called before the first frame update
     void Start()
     {
         CheckPlayerType();
+
         rb = GetComponent<Rigidbody>();
+
+
+        if (photonView.IsMine)
+        {
+            pooledObjects = new List<GameObject>();
+            for (int i = 0; i < amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(CollisionEffectPrefab, Vector3.zero, Quaternion.identity);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetPooledObject()
     {
-        
+
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        return null;
+    }
+
+    IEnumerator DeactivateAfterSeconds(GameObject _gameObject, float _seconds)
+    {
+        yield return new WaitForSeconds(_seconds);
+        _gameObject.SetActive(false);
+
     }
 }
+
